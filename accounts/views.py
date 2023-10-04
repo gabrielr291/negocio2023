b7051e6877b95969e71fe7c228e69c139ebfd877
Gabriel Riveros <gabriel.omar.r29@gmail.com>
2023-10-03 22:54:13 -0300

    Cambios antes del requirements

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
-from .forms import RegistrationForm
-from .models import Account
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import RegistrationForm,UserProfileForm, UserForm
+from .models import Account, UserProfile
+from orders.models import Order
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 from django.contrib.sites.shortcuts import get_current_site
@@ -9,6 +10,9 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.auth.tokens import PasswordResetTokenGenerator,default_token_generator
 from django.utils.encoding import force_bytes
 from django.core.mail import EmailMessage
+from carts.views import _cart_id
+from carts.models import Cart,CartItem
+import requests
 
 # Create your views here.
 def register(request):
@@ -26,6 +30,13 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name,last_name=last_name,email=email,username=username,password=password)
             user.phone_number = phone_number
             user.save()
+
+            profile = UserProfile()
+            profile.user_id = user.id
+            profile.profile_picture = 'default/default-user.png'
+            profile.save()
+
+
             token_generator = PasswordResetTokenGenerator()
             current_site=get_current_site(request)
             mail_subject = 'Por favor activa tu cuenta en la tienda Online'
@@ -58,14 +69,62 @@ def login(request):
         user= auth.authenticate(email=email,password=password)
 
         if user is not None:
+
+            try:
+                cart = Cart.objects.get(cart_id = _cart_id(request))
+                is_cart_item_exists= CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item= CartItem.objects.filter(cart=cart)
+
+                    product_variation= []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+                    
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id =[]
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item= CartItem.objects.get(id=item_id)
+                            item.quantity +=1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+            except:
+                pass
+
+
             auth.login(request,user)
             messages.success(request,'Has iniciado sesion exitosamente')
-            return redirect('dashboard')
+
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query  = requests.utils.urlparse(url).query
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
+
         else:
             messages.error(request,'Las credenciales son incorrectas')
             return redirect('login')
         
     return render(request, 'accounts/login.html')
+
 @login_required(login_url='login')
 def logout(request):
     auth.logout(request)
@@ -90,7 +149,16 @@ def activate(request,uidb64,token):
 
 @login_required(login_url='login')    
 def dashboard(request):
-    return render(request,'accounts/dashboard.html')
+    orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
+    orders_count = orders.count()
+
+    userprofile = UserProfile.objects.get(user_id=request.user.id)
+    context = {
+        'orders_count': orders_count,
+        'userprofile' : userprofile,
+    }
+
+    return render(request,'accounts/dashboard.html',context)
 
 def forgotPassword(request):
     if request.method == 'POST':
@@ -150,8 +218,57 @@ def resetPassword(request):
     else:
         return render(request,'accounts/resetPassword.html')
 
+def my_orders(request):
+    orders = Order.objects.filter(user=request.user, is_ordered=True).order_by('-created_at')
+    context ={
+        'orders': orders,
+    }
+    return render(request,'accounts/my_orders.html', context)
 
+@login_required(login_url='login')
+def edit_profile(request):
+    userprofile = get_object_or_404(UserProfile, user=request.user)
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=userprofile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'su informacion fue guarada con exito')
+            return redirect('edit_profile')
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = UserProfileForm(instance=userprofile)
 
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+        'userprofile' : userprofile,
+    }
+    return render(request, 'accounts/edit_profile.html',context)
 
+@login_required(login_url='login')
+def change_password(request):
+    if request.method == 'POST':
+        current_password = request.POST['current_password']
+        new_password = request.POST['new_password']
+        confirm_password = request.POST['confirm_password']
 
+        user = Account.objects.get(username__exact=request.user.username)
 
+        if new_password == confirm_password:
+            success = user.check_password(current_password)
+            if success:
+                user.set_password(new_password)
+                user.save()
+
+                messages.success(request, 'Su contraseña se ha actualizado exitosamente')
+                return redirect('change_password')
+            else:
+                messages.error(request,'por favor ingrese una contraseña valida')
+                return redirect('change_password')
+        else:
+            messages.error(request, 'La contraseña no coincide con la confirmacion')
+            return redirect('change_password')
+        
+    return render(request,'accounts/change_password.html')
